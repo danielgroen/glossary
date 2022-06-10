7811756622f11b334e97830a1d86a9d604cee5a2
Daniel Groen <daniel@MacBook-Pro.localdomain>
2022-06-10 13:41:55 +0200

    up

--- src/index.php
+++ src/index.php
@@ -84,7 +84,7 @@ function create_begrippen_taxonomy()
     'menu_name' => __('Begrip Categorieën'),
   );
 
-  register_taxonomy('glossary_category', ['begrippen', 'blog', 'page'], array(
+  register_taxonomy('glossary_category', ['begrippen', 'page'], array(
     'hierarchical' => false,
     'meta_box_cb' => 'post_categories_meta_box',
     'labels' => $labels,
